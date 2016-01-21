80c601ec7e92c0743338944d57739b2e68fd1bd9
thospfuller <thomas.fuller@coherentlogic.com>
2016-01-21 15:34:30 -0500

    Addressed the package name change for the AbstractQueryBuilder.

--- fred-client-core/src/main/java/com/coherentlogic/fred/client/core/builders/QueryBuilder.java
+++ fred-client-core/src/main/java/com/coherentlogic/fred/client/core/builders/QueryBuilder.java
@@ -34,7 +34,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
-import com.coherentlogic.coherent.data.model.core.builders.AbstractQueryBuilder;
+import com.coherentlogic.coherent.data.model.core.builders.rest.AbstractQueryBuilder;
 import com.coherentlogic.coherent.data.model.core.cache.CacheServiceProviderSpecification;
 import com.coherentlogic.fred.client.core.domain.AggregationMethod;
 import com.coherentlogic.fred.client.core.domain.FileType;
