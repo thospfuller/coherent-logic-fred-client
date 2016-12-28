0203161736d470031d7de54c9190efa4b05a74de
thospfuller <thomas.fuller@coherentlogic.com>
2016-12-28 12:39:23 -0500

    Added documentation and changed the package name.

--- fred-client-misc/src/main/java/com/coherentlogic/fred/client/domain/AggregationMethod.java
+++ fred-client-misc/src/main/java/com/coherentlogic/fred/client/domain/AggregationMethod.java
@@ -1,10 +1,11 @@
-package com.coherentlogic.fred.client.core.domain;
+package com.coherentlogic.fred.client.domain;
 
 /**
  * A key that indicates the aggregation method used for frequency aggregation.
  * This parameter has no affect if the frequency parameter is not set.
  *
  * @see <a href="https://api.stlouisfed.org/docs/fred/series_observations.html#aggregation_method">Aggregation Method</a>
+ * @see <a href="https://research.stlouisfed.org/docs/api/geofred/regional_data.html#aggregation_method">aggregation_method</a>
  *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
