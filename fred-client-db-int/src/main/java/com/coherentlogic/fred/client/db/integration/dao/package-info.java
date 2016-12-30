052988729413b55fdf5e8ccb6251cadb03347ab2
thospfuller <thomas.fuller@coherentlogic.com>
2016-12-29 23:53:00 -0500

    Documentation has changed.

--- fred-client-db-int/src/main/java/com/coherentlogic/fred/client/db/integration/dao/package-info.java
+++ fred-client-db-int/src/main/java/com/coherentlogic/fred/client/db/integration/dao/package-info.java
@@ -1,6 +1,4 @@
 /**
- * This package contains classes that follow the data access pattern and allow
- * create, read, update, and delete operations to be performed on the FRED
- * Client's domain model.
+ * This package contains specifications for various repositories that pertain to the FRED Client's domain model.
  */
 package com.coherentlogic.fred.client.db.integration.dao;
